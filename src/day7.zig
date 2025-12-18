5c3277220988903d706201d2061aec47b9174ae7
K. Adam Christensen <pope@shifteleven.com>
2025-12-18 09:29:22 -0800

    Day 7 part 2
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- src/day7.zig
+++ src/day7.zig
@@ -6,12 +6,16 @@ pub fn main() !void {
     defer setup.deinit();
 
     var it = setup.lineIterator();
-    std.debug.print(
-        "07.1: Splits = {}\n",
-        .{try part1Answer(setup.allocator(), &it)},
-    );
+    var arena = std.heap.ArenaAllocator.init(setup.allocator());
+    defer arena.deinit();
 
-    std.debug.print("07.1: TODO\n", .{});
+    _, const p1a = try part1Answer(&arena, &it);
+    std.debug.print("07.1: Splits = {}\n", .{p1a});
+
+    try setup.reset();
+    _ = arena.reset(.retain_capacity);
+    const p2a = try part2Answer(&arena, &it);
+    std.debug.print("07.2: Timelines = {}\n", .{p2a});
 }
 
 const Tile = enum {
@@ -21,7 +25,90 @@ const Tile = enum {
     beam,
 };
 
-fn part1Answer(alloc: std.mem.Allocator, it: *aoc.LineIterator) !u64 {
+fn part1Answer(
+    arena: *std.heap.ArenaAllocator,
+    it: *aoc.LineIterator,
+) !struct {
+    []const []const Tile,
+    u64,
+} {
+    const data = try parseInput(arena, it);
+
+    var tmp_buf = try arena.allocator().alloc(Tile, data[0].len);
+    @memset(tmp_buf, .empty);
+
+    var splits: u64 = 0;
+    var prev = data[0];
+    for (data) |row| {
+        for (row, prev, 0..) |tile, p_tile, i| {
+            switch (tile) {
+                .empty, .beam => {},
+                .start => tmp_buf[i] = .beam,
+                .splitter => if (p_tile == .beam) {
+                    std.debug.assert(i > 0 and i < tmp_buf.len - 1);
+
+                    splits += 1;
+                    tmp_buf[i - 1] = .beam; // left
+                    tmp_buf[i] = .empty; // cur
+                    tmp_buf[i + 1] = .beam; // right
+                },
+            }
+        }
+        for (row, tmp_buf) |*tile, b_tile| {
+            if (tile.* == .empty and b_tile == .beam) tile.* = .beam;
+        }
+        prev = row;
+    }
+
+    return .{ data, splits };
+}
+
+fn part2Answer(
+    arena: *std.heap.ArenaAllocator,
+    it: *aoc.LineIterator,
+) !u64 {
+    const data = try parseInput(arena, it);
+
+    const TimelineCounter = struct {
+        tile: Tile,
+        count: u64,
+    };
+    var tmp_buf = try arena.allocator().alloc(
+        TimelineCounter,
+        data[0].len,
+    );
+    for (tmp_buf) |*b| b.* = .{ .tile = .empty, .count = 0 };
+
+    var prev = data[0];
+    for (data) |row| {
+        for (row, prev, 0..) |tile, p_tile, i| {
+            switch (tile) {
+                .empty, .beam => {},
+                .start => tmp_buf[i] = .{ .tile = .beam, .count = 1 },
+                .splitter => if (p_tile == .beam) {
+                    tmp_buf[i - 1].tile = .beam;
+                    tmp_buf[i - 1].count += tmp_buf[i].count;
+                    tmp_buf[i + 1].tile = .beam;
+                    tmp_buf[i + 1].count += tmp_buf[i].count;
+
+                    tmp_buf[i] = .{ .tile = .empty, .count = 0 };
+                },
+            }
+        }
+        for (row, tmp_buf) |*tile, b_tile| {
+            if (tile.* == .empty and b_tile.tile == .beam) tile.* = .beam;
+        }
+        prev = row;
+    }
+
+    var timelines: u64 = 0;
+    for (tmp_buf) |b| timelines += b.count;
+    return timelines;
+}
+
+fn parseInput(arena: *std.heap.ArenaAllocator, it: *aoc.LineIterator) ![][]Tile {
+    const alloc = arena.allocator();
+
     const line_len = blk: {
         if (try it.peek()) |l| {
             if (l.len == 0) return error.Oops;
@@ -30,50 +117,30 @@ fn part1Answer(alloc: std.mem.Allocator, it: *aoc.LineIterator) !u64 {
         return error.Oops;
     };
 
-    var prev = try alloc.alloc(Tile, line_len);
-    defer alloc.free(prev);
-
-    var cur = try alloc.alloc(Tile, line_len);
-    defer alloc.free(cur);
-
-    var tmp = try alloc.alloc(Tile, line_len);
-    defer alloc.free(tmp);
-    @memset(tmp, .empty);
-
-    var splits: u64 = 0;
+    var data: std.ArrayList([]Tile) = .empty;
     while (try it.next()) |line| {
-        for (line, cur, prev, 0..) |char, *c, p, i| {
-            c.* = switch (char) {
+        if (line_len != line.len) return error.Oops;
+        var row: std.ArrayList(Tile) = try .initCapacity(alloc, line_len);
+        for (line) |c| {
+            try row.appendBounded(switch (c) {
                 '.' => .empty,
-                'S' => blk: {
-                    tmp[i] = .beam;
-                    break :blk .start;
-                },
-                '^' => blk: {
-                    if (p == .beam) {
-                        splits += 1;
-                        tmp[i] = .empty;
-                        if (i > 0) tmp[i - 1] = .beam;
-                        if (i < tmp.len - 1) tmp[i + 1] = .beam;
-                    }
-                    break :blk .splitter;
-                },
+                'S' => .start,
+                '^' => .splitter,
                 else => return error.Oops,
-            };
+            });
         }
-        for (cur, tmp) |*t, b| {
-            if (t.* == .empty and b == .beam) t.* = .beam;
-        }
-
-        const tmp_swp = cur;
-        cur = prev;
-        prev = tmp_swp;
+        try data.append(alloc, try row.toOwnedSlice(alloc));
     }
-    return splits;
+    return data.toOwnedSlice(alloc);
 }
 
-fn testTilesToStr(alloc: std.mem.Allocator, tiles: *[16][15]Tile) ![]u8 {
-    var str: std.ArrayList(u8) = try .initCapacity(alloc, 16 * 16);
+fn tilesToStr(alloc: std.mem.Allocator, tiles: []const []const Tile) ![]u8 {
+    if (tiles.len == 0) return error.Oops;
+
+    var str: std.ArrayList(u8) = try .initCapacity(
+        alloc,
+        tiles.len * (tiles[0].len + 1),
+    );
     defer str.deinit(alloc);
 
     for (tiles) |row| {
@@ -111,38 +178,13 @@ const test_input =
     \\...............
 ;
 
-test "day 7 - part 1.0" {
-    var output: [16][15]Tile = undefined;
-    var buf: [15]Tile = @splat(.empty);
+test "day 7 - part 1" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
 
     var it = aoc.LineIterator.initFromBuffer(test_input);
-    var idx: usize = 0;
-    var splits: u8 = 0;
-    while (try it.next()) |line| : (idx += 1) {
-        const tiles = &output[idx];
-        for (line, tiles, 0..) |c, *t, cur| {
-            t.* = switch (c) {
-                '.' => .empty,
-                'S' => blk: {
-                    buf[cur] = .beam;
-                    break :blk .start;
-                },
-                '^' => blk: {
-                    if (idx > 0 and output[idx - 1][cur] == .beam) {
-                        splits += 1;
-                        buf[cur] = .empty;
-                        if (cur > 0) buf[cur - 1] = .beam;
-                        if (cur < buf.len - 1) buf[cur + 1] = .beam;
-                    }
-                    break :blk .splitter;
-                },
-                else => return error.Oops,
-            };
-        }
-        for (tiles, &buf) |*t, b| {
-            if (t.* == .empty and b == .beam) t.* = .beam;
-        }
-    }
+
+    const data, const splits = try part1Answer(&arena, &it);
 
     const expected =
         \\.......S.......
@@ -162,9 +204,9 @@ test "day 7 - part 1.0" {
         \\|^|^|^|^|^|||^|
         \\|.|.|.|.|.|||.|
     ;
-    const actual = try testTilesToStr(
+    const actual = try tilesToStr(
         std.testing.allocator,
-        &output,
+        data,
     );
     defer std.testing.allocator.free(actual);
 
@@ -172,10 +214,12 @@ test "day 7 - part 1.0" {
     try std.testing.expectEqual(21, splits);
 }
 
-test "day 7 - part 1.1" {
+test "day 7 - part 2" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+
     var it = aoc.LineIterator.initFromBuffer(test_input);
-    try std.testing.expectEqual(
-        21,
-        try part1Answer(std.testing.allocator, &it),
-    );
+
+    const timelines = try part2Answer(&arena, &it);
+    try std.testing.expectEqual(40, timelines);
 }
